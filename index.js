da5d78e1dbb26a37e930e5c8184130ba0cc16aa5
Maulana <smamalik33@gmail.com>
2023-09-19 22:18:22 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require("express")
+const request = require("request-promise")
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+
+const generateScraperUrl = (apiKey) => `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
+
+
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.send('Welcome to Amazon Scraper Api');
+})
+
+//Get Product detail
+app.get('/products/:productId', async(req, res)=>{
+    const { productId } = req.params;
+    const { api_key } = req.query;
+
+    try {
+        const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/dp/${productId}`);
+
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.sendStatus(404).json(error)
+    }
+})
+
+//Get Product detail reviews
+app.get('/products/:productId/reviews', async(req, res)=>{
+    const { productId } = req.params;
+    const { api_key } = req.query;
+
+    try {
+        const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/product-reviews/${productId}`);
+
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.sendStatus(404).json(error)
+    }
+})
+
+//Get Product offers
+app.get('/products/:productId/offers', async(req, res)=>{
+    const { productId } = req.params;
+    const { api_key } = req.query;
+
+    try {
+        const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.sendStatus(404).json(error)
+    }
+})
+
+//Get Search results
+app.get('/search/:searchQuery', async(req, res)=>{
+    const { searchQuery } = req.params;
+    const { api_key } = req.query;
+
+    try {
+        const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/s?k=${searchQuery}`);
+
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.sendStatus(404).json(error)
+    }
+})
+
+
+app.listen(PORT, ()=> console.log(`Server running on Port ${PORT}`))
+
+
